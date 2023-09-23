f00d149a2b36a2422fc848e837cbd91fd834b6d4
finbargiusti <finbargiusti@gmail.com>
2023-09-23 11:15:55 +0100

    add frameworks

--- SchemaScraper.py
+++ SchemaScraper.py
@@ -1,9 +1,13 @@
+import sqlite3.Connection
+
 def SchemaScraper():
 
     # TODO: maybe add strategy injection
-
-    def __init__(self):
+    def __init__(self, db: Connection):
         return
+
+    def get_schema() -> str:
+        return ""
 
 
 if __name__ == "__main__":
