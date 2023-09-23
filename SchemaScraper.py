b72d0dc5587620be16df63eb99c9d9b0cd7113c0
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-09-23 11:35:09 +0100

    fixed minor syntax issues

--- SchemaScraper.py
+++ SchemaScraper.py
@@ -1,14 +1,19 @@
 import sqlite3
 
-def SchemaScraper():
+
+class SchemaScraper:
 
     # TODO: maybe add strategy injection
     def __init__(self, db: sqlite3.Connection):
         return
 
-    def get_schema() -> str:
+    def connect_function(self, name: str):
+        return sqlite3.connect(name)
+
+    def get_schema(self) -> str:
         return ""
 
 
 if __name__ == "__main__":
     # some testing code
+    pass
