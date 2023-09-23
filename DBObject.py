ef323d3bcc18d74d2a1f7d1b56938133fcc1496a
finbargiusti <finbargiusti@gmail.com>
2023-09-23 13:16:06 +0100

    complete prompt generation

--- DBObject.py
+++ DBObject.py
@@ -1,15 +1,30 @@
-import SchemaScraper
-import sqlite3
+from langchain.utilities import SQLDatabase
+from langchain.prompts import ChatPromptTemplate
+from template import generateTemplate
 
-def DBObject():
 
-    db: sqlite3.Connection
+class DBObject:
+    db: SQLDatabase
+    prompt_template: ChatPromptTemplate
 
-    def __init__(self, db: sqlite3.Connection):
-
+    def __init__(self, db: SQLDatabase):
+        self.prompt_template = generateTemplate()
         self.db = db
 
     def get_schema(self) -> str:
-        scraper = SchemaScraper(db)
-        return scraper.get_schema()
+        return self.db.get_table_info()
 
+    def generate_prompt(self, question: str, hints: list[str]) -> str:
+        return self.prompt_template.format_prompt(
+            schema=self.get_schema(), question=question, hints=", ".join(hints)
+        ).to_string()
+
+
+if __name__ == "__main__":
+    # attempt to generate
+
+    db = SQLDatabase.from_uri("sqlite:///sampleDatabases/Northwind3.db?mode=ro")
+
+    db_object = DBObject(db)
+
+    print(db_object.generate_prompt("How many users have age over 45?", ["users"]))
